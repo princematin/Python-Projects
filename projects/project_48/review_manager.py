efd2b6c7a52729f596da6743a9bcfb252b914371
Prince <matinx636@gmail.com>
2026-09-11 00:36:17 +0330

    project48 : file review_manager

--- projects/project_48/review_manager.py
+++ projects/project_48/review_manager.py
@@ -9,22 +9,31 @@ class ReviewManager:
         self.session = session
 
     def create(self, movie_id: int, user_id: int, rating: int, comment: str = None) -> Review:
-        pass
-
+        review = Review(
+            movie_id= movie_id,
+            user_id= user_id,
+            rating= rating,
+            comment= comment
+        )
+        self.session.add(review)
+        self.session.commit()
+        return review
+    
     def get(self, review_id: int) -> Review | None:
-        pass
+        return self.session.get(Review, review_id)
         
     def get_all(self):
-        pass
+        return list(self.session.execute(select(Review)).scalars())
 
     def get_reviews_by_user(self, user_id: int):
-        pass
+        return list(self.session.execute(select(Review).where(Review.user_id == user_id)).scalars())
 
     def get_latest_reviews_for_movie_by_time(self, movie_id: int, limit: int = 5):
-        pass
-
+        return list(self.session.execute(select(Review).where(Review.movie_id == movie_id).order_by((Review.created_at).desc()).limit(limit)).scalars())
+     
     def get_highest_rated_reviews(self, movie_id: int, limit: int = 5):
-        pass
+        return list(self.session.execute(select(Review).where(Review.movie_id == movie_id).order_by((Review.rating).desc()).limit(limit)).scalars())
 
     def get_average_rating_by_user(self):
-        pass
+        return list(self.session.execute(select(Review.user_id, func.avg(Review.rating)).group_by(Review.user_id)))
+
