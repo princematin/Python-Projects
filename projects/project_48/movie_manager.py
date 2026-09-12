91f1984cb3d24bc8f8a0fa462306bdd71eac7a5d
Prince <matinx636@gmail.com>
2026-09-12 21:36:35 +0330

    project48 : movie_manager

--- projects/project_48/movie_manager.py
+++ projects/project_48/movie_manager.py
@@ -39,19 +39,42 @@ class MovieManager:
         return score
 
     def update(self, movie_id: int, update_data: dict) -> Movie:
-        pass
+        result  = self.session.get(Movie, movie_id)
+        if not result:
+            return None
+        for key, value in update_data.items():
+            setattr(result, key, value)
+        self.session.commit()
+        return result
 
     def delete(self, movie_id: int) -> bool:
-        pass
+        result = self.session.get(Movie, movie_id)
+        if not result:
+            return False
+        self.session.delete(result)
+        self.session.commit()
+        return True
 
     def remove_genre(self, movie_id: int, genre: Genre) -> Movie:
-        pass
+        result = self.session.get(Movie, movie_id)
+        if not result:
+            return None
+        if genre in result.genres:
+            result.genres.remove(genre)
+        self.session.commit()
+        return result
 
     def get_top_movies_by_rating(self, limit: int = 10) -> list[tuple]:
-        pass
+        return list(self.session.execute(select(Movie, func.avg(Review.rating)).join(Movie.reviews).group_by(Movie).order_by(func.avg(Review.rating).desc()).limit(limit)))
+    
 
     def get_movies_by_genre(self, genre_name: str) -> list[Movie]:
-        pass
+        return list(self.session.execute(select(Movie).join(Movie.genres).where(Genre.name == genre_name)).scalars())
 
     def get_top_rated_movies_by_genre(self):
-        pass
+        result = select(Genre.name,Movie.title,func.avg(Review.rating).label("average_rating")).join(Genre.movies).join(Movie.reviews).group_by(Genre.name,Movie.title).order_by(func.avg(Review.rating).desc())
+        result = result.subquery()
+        top_rating = select(result.c.name,func.max(result.c.average_rating).label("max_rating")).group_by(result.c.name)
+        top_rating = top_rating.subquery()
+        final_result = select(result.c.name,result.c.title,result.c.average_rating).join(top_rating,(result.c.name == top_rating.c.name) & (result.c.average_rating == top_rating.c.max_rating)).order_by(result.c.name,result.c.average_rating.desc())
+        return list(self.session.execute(final_result))
